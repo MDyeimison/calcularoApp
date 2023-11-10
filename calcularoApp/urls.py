f7f9ca4a7aca18d37327f4f870c08ce86d4f3c0c
MDyeimison <marcosdyeimison@gmail.com>
2023-11-10 15:05:54 -0300

    add rota

--- calcularoApp/urls.py
+++ calcularoApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from calcularoApp.views import CalculateView
+
 urlpatterns = [
-    path('calcularo/', Calculate.as_view(), name='calculadora'),
+    path('calcularo/<str:val1>/<str:val2>/<str:op>', CalculateView.as_view(), name='calculadora'),
 ]
--- calcularoApp/views.py
+++ calcularoApp/views.py
@@ -1,30 +1,29 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
-# class CalculateView(TemplateView):
-    # model = Calculadora
+from calcularoApp.models import Calculadora
 
-    # def get(self, *args, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     periodo = TermoCompromissoPeriodo.objects.get(id=kwargs['duplicados'])
-    #     termoPeriodo = periodo.termo
-    #     termos = TermoCompromisso.objects.filter(aluno__pessoa__nomeCompleto=termoPeriodo.aluno.pessoa.nomeCompleto, semestre=termoPeriodo.semestre, disciplina=termoPeriodo.disciplina).order_by('dataCadastro')
-    #     periodos = [termo.retornaUltimoPeriodo() for termo in termos]
-    #     termosDuplicados = [p.termo for p in periodos if p.podeSerExcluido()!=None]
-    #     pessoa = periodo.termo.aluno.pessoa.nomeCompleto
-    #     tcesList = []
-    #     for termo in termosDuplicados:
-    #         termoDisplay = str(termo._get_FIELD_display.__self__)
-    #         numero = str(termo.numero)
-    #         dados = {
-    #             "id":termo.id,
-    #             "termo":termoDisplay,
-    #             "numero":f'{numero[:5]}.{numero[5:]}',
-    #             "nome":pessoa,
-    #             "dataCadastro":termo.dataCadastro.strftime("%d/%m/%Y"),
-    #             "quemCadastrou": termo.quemCadastrou.nomeCompleto if termo.quemCadastrou else '',
-    #         }
-    #         tcesList.append(dados)
-    #     data = {"nome":pessoa, "termos":tcesList}
-    #     return JsonResponse(data)
+# Create your views here.
+class CalculateView(TemplateView):
+    model = Calculadora
+
+    def get(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        val1 = float(kwargs['val1'])
+        val2 = float(kwargs['val2'])
+        op = kwargs['op']
+
+        if op == 'ad':
+            result = val1+val2
+        elif op == 'sub':
+            result = val1-val2
+        elif op == 'mult':
+            result = val1*val2
+        elif op == 'div':
+            result = float(val1/val2)
+        elif op == 'exp':
+            result = val1**val2
+
+        print(f'{val1} {op} {val2} = {result}')
+        return JsonResponse({"resultado":result})
